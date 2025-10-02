84d4edbf19f55ffa3e00f5f0377440f7504edd2f
Alimeh <135833550+MarcoMeh@users.noreply.github.com>
2025-10-02 10:41:45 +0000

    app tsx remove routure

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom"; // IMPORT BrowserRouter
+import { Routes, Route } from "react-router-dom"; // IMPORT BrowserRouter
 import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from './components/LanguageSwitcher';
 
@@ -46,7 +46,6 @@ const App = () => {
         */}
 
         {/* BrowserRouter is ESSENTIAL for react-router-dom to work */}
-        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/products" element={<Products />} />
@@ -56,7 +55,6 @@ const App = () => {
             <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
       </TooltipProvider>
     </QueryClientProvider>
   );
